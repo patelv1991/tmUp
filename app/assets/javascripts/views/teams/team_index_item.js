cb3ad9858d1f3ee1a67b34bef8bf17e350f8c8a8
Varun Patel <patelv1991@gmail.com>
2015-08-21 00:55:49 -0700

    added css for team members to sidebar, minor changes to projects container and top navbar

--- app/assets/javascripts/views/teams/team_index_item.js
+++ app/assets/javascripts/views/teams/team_index_item.js
@@ -2,9 +2,24 @@ TmUp.Views.TeamIndexItem = Backbone.View.extend({
   template: JST['teams/index_item'],
   tagName: 'li',
 
+  // initialize: function () {
+  //   this.listenTo(this.model, 'sync', this.addRandomColorToInitials);
+  // },
+
   render: function () {
-    var content = this.template({ team_member: this.model });
+    var content = this.template({ team_member: this.model, randomColor: this.randomColor() });
     this.$el.html(content);
+    // this.addRandomColorToInitials();
     return this;
+  },
+
+  randomColor: function () {
+    var x = Math.round(0xffffff * Math.random()).toString(16);
+    var y = (6-x.length);
+    var z = '000000';
+    var z1 = z.substring(0,y);
+    return "#" + z1 + x;
+    // debugger
+    // $('.sidebar-nav div > div ul.team-members li a').css({"background-color": randomColor});
   }
 });
