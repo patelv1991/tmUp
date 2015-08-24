b83750efd9c5a48ffed2fef2bfc10d5252cb96ac
Varun Patel <patelv1991@gmail.com>
2015-08-23 22:13:16 -0700

    Backbone: Add server response parser to project model

--- app/assets/javascripts/models/project.js
+++ app/assets/javascripts/models/project.js
@@ -1,4 +1,18 @@
 TmUp.Models.Project = Backbone.Model.extend({
-  urlRoot: 'api/projects'
+  urlRoot: 'api/projects',
 
+  tasks: function () {
+    if (!this._tasks) {
+      this._tasks = new TmUp.Collections.Tasks([], { user: this });
+    }
+    return this._tasks;
+  },
+
+  parse: function (response) {
+    if (response.tasks) {
+      this.tasks().set(response.tasks, { parse: true });
+      delete response.tasks;
+    }
+    return response;
+  }
 });
