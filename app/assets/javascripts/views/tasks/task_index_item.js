33351e6eb73b5f01f848ab2510d17a9856428fcf
Varun Patel <patelv1991@gmail.com>
2015-08-24 13:47:10 -0700

    Backbone: add work team members to dropdown

--- app/assets/javascripts/views/tasks/task_index_item.js
+++ app/assets/javascripts/views/tasks/task_index_item.js
@@ -6,10 +6,19 @@ TmUp.Views.TaskIndexItem = Backbone.View.extend({
     this.workspace = options.workspace;
   },
 
+  renderAssignee: function () {
+    var assigneeId = parseInt(this.model.escape('assignee_id'));
+    var assignee = this.workspace.workTeam().findWhere({ id: assigneeId });
+    return assignee.escape('fname')[0].toUpperCase() +
+                assignee.escape('lname')[0].toUpperCase();
+  },
+
   render: function () {
     var content = this.template({
       task: this.model,
-      workspace: this.workspace
+      workspace: this.workspace,
+      assignment: this.renderAssignee(),
+      randomColor: this.model.color
     });
     this.$el.html(content);
     return this;
