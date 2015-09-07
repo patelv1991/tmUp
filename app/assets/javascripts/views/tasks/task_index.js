7405aaaad561b8d2cd0ffd17dc6dd3282d4c144a
Varun Patel <patelv1991@gmail.com>
2015-09-06 23:25:36 -0700

    Add more seed data

--- app/assets/javascripts/views/tasks/task_index.js
+++ app/assets/javascripts/views/tasks/task_index.js
@@ -8,7 +8,7 @@ TmUp.Views.TaskIndex = Backbone.CompositeView.extend({
     this.renderingAllTasks = options.renderingAllTasks;
     // this.listenTo(this.user, 'sync', this.render);
     this.listenTo(this.collection, 'add', this.addTask);
-    // this.listenTo(this.collection, 'sync add change', this.render);
+    // this.listenTo(this.collection, 'sync', this.render);
 
     // This makes sure that project title is updated when project is updated
     this.project && this.listenTo(this.project, 'change', this.render);
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,166 @@
 User.create({ fname: "Guest", lname: "User", email: "guest@tmup.work", password: "password" })
 User.create({ fname: "Varun", lname: "Patel", email: "patelv1991@gmail.com", password: "123456" })
 
+# TmUp features data
+Workspace.create({ title: "TmUp Features" })
+
+# Add team members to workspace
+2.times do |i|
+  i += 1
+  UserWorkspace.create({ user_id: i, workspace_id: Workspace.find_by_title("TmUp Features").id })
+end
+
+# TmUp Features projects
+Project.create({
+  title: "Current Features",
+  description: "This Project lists current features of TmUp.",
+  workspace_id: Workspace.find_by_title("TmUp Features").id
+})
+
+Project.create({
+  title: "Features Under Development",
+  description: "This Project lists features I am currently working on.",
+  workspace_id: Workspace.find_by_title("TmUp Features").id
+})
+
+# TmUp Current feature tasks
+Task.create({
+  title: "Custom user authentication using BCrypt to store password hash",
+  project_id: Project.find_by_title("Current Features").id,
+  creator_id: 1,
+  assignee_id: User.find_by_fname("Varun").id,
+  due_date: "2015-08-13"
+})
+
+Task.create({
+  title: "Create / delete workspace",
+  project_id: Project.find_by_title("Current Features").id,
+  creator_id: 1,
+  assignee_id: User.find_by_fname("Varun").id,
+  due_date: "2015-08-14"
+})
+
+Task.create({
+  title: "Add / remove people from workspace, leave workspace",
+  project_id: Project.find_by_title("Current Features").id,
+  creator_id: 1,
+  assignee_id: User.find_by_fname("Varun").id,
+  due_date: "2015-08-14"
+})
+
+Task.create({
+  title: "Validate email and provide dynamic feedback through the email input box",
+  project_id: Project.find_by_title("Current Features").id,
+  creator_id: 1,
+  assignee_id: User.find_by_fname("Varun").id,
+  due_date: "2015-08-15"
+})
+
+Task.create({
+  title: "Create / edit / delete project",
+  project_id: Project.find_by_title("Current Features").id,
+  creator_id: 1,
+  assignee_id: User.find_by_fname("Varun").id,
+  due_date: "2015-08-15"
+})
+
+Task.create({
+  title: "Add / delete tasks",
+  project_id: Project.find_by_title("Current Features").id,
+  creator_id: 1,
+  assignee_id: User.find_by_fname("Varun").id,
+  due_date: "2015-08-16"
+})
+
+Task.create({
+  title: "Edit tasks by double clicking the task",
+  project_id: Project.find_by_title("Current Features").id,
+  creator_id: 1,
+  assignee_id: User.find_by_fname("Varun").id,
+  due_date: "2015-08-16"
+})
+
+Task.create({
+  title: "Assign task to a team member and add a due date",
+  project_id: Project.find_by_title("Current Features").id,
+  creator_id: 1,
+  assignee_id: User.find_by_fname("Varun").id,
+  due_date: "2015-08-16",
+  completed: true
+})
+
+Task.create({
+  title: "Allow users to see tasks assigned to them by clicking on 'MY TASKS' in the sidebar",
+  project_id: Project.find_by_title("Current Features").id,
+  creator_id: 1,
+  assignee_id: User.find_by_fname("Varun").id,
+  due_date: "2015-08-17",
+  completed: true
+})
+
+Task.create({
+  title: "Allow users to see other people's tasks by clicking on user's profile",
+  project_id: Project.find_by_title("Current Features").id,
+  creator_id: 1,
+  assignee_id: User.find_by_fname("Varun").id,
+  due_date: "2015-08-17"
+})
+
+Task.create({
+  title: "Allow users to mark tasks as complete and see completed",
+  project_id: Project.find_by_title("Current Features").id,
+  creator_id: 1,
+  assignee_id: User.find_by_fname("Varun").id,
+  due_date: "2015-08-18"
+})
+
+Task.create({
+  title: "Implement new features on this project",
+  project_id: Project.find_by_title("Current Features").id,
+  creator_id: 1,
+  assignee_id: User.find_by_fname("Varun").id,
+  due_date: Time.now.localtime
+})
+
+Task.create({
+  title: "Check out all TmUp features!!!!",
+  project_id: Project.find_by_title("Current Features").id,
+  creator_id: 1,
+  assignee_id: User.find_by_fname("Guest").id,
+  due_date: Time.now.localtime
+})
+
+Task.create({
+  title: "Check out the website tour on top right corner",
+  project_id: Project.find_by_title("Current Features").id,
+  creator_id: 1,
+  assignee_id: User.find_by_fname("Guest").id,
+  due_date: Time.now.localtime
+})
+
+Task.create({
+  title: "Implement search feature to allow users to query tasks",
+  project_id: Project.find_by_title("Features Under Development").id,
+  creator_id: 1,
+  assignee_id: User.find_by_fname("Varun").id,
+  due_date: Time.now.localtime + 10.days
+})
+
+Task.create({
+  title: "Add task show page",
+  project_id: Project.find_by_title("Features Under Development").id,
+  creator_id: 1,
+  assignee_id: User.find_by_fname("Varun").id,
+  due_date: Time.now.localtime + 12.days
+})
+
+Task.create({
+  title: "Allow users to create nested subtasks",
+  project_id: Project.find_by_title("Features Under Development").id,
+  creator_id: 1,
+  assignee_id: User.find_by_fname("Varun").id,
+  due_date: Time.now.localtime + 15.days
+})
 # Avengers data
 # Avengers workspace
 Workspace.create({ title: "Avengers" })
@@ -32,8 +192,7 @@ end
 
 # Add Avengers projects
 Project.create({
-  title: "Hydra",
-  description: "Find every last hydra agent within SHIELD and locate Red Skull",
+  title: "Avengers Tower",
   workspace_id: Workspace.find_by_title("Avengers").id
 })
 
@@ -43,11 +202,28 @@ Project.create({
 })
 
 Project.create({
-  title: "Avengers Tower",
+  title: "Hydra",
+  description: "Find every last hydra agent within SHIELD and locate Red Skull",
   workspace_id: Workspace.find_by_title("Avengers").id
 })
 
 # Add Project Hydra Tasks
+Task.create({
+  title: "Finish designing newest avengers tower",
+  project_id: Project.find_by_title("Avengers Tower").id,
+  creator_id: 1,
+  assignee_id: User.find_by_fname("Guest").id,
+  due_date: 5.days.ago.localtime
+})
+
+Task.create({
+  title: "Locate all six infinity stones",
+  project_id: Project.find_by_title("Infinity Stones").id,
+  creator_id: 1,
+  assignee_id: User.find_by_fname("Guest").id,
+  due_date: 1.day.ago.localtime
+})
+
 Task.create({
   title: "DO NOT engage with enemy. Leave that to Avengers!!!",
   project_id: Project.find_by_title("Hydra").id,
@@ -123,14 +299,6 @@ Task.create({
 
 # Add Project Infinity Stone Tasks
 Task.create({
-  title: "Locate all six infinity stones",
-  project_id: Project.find_by_title("Infinity Stones").id,
-  creator_id: 1,
-  assignee_id: User.find_by_fname("Guest").id,
-  due_date: 1.day.ago.localtime
-})
-
-Task.create({
   title: "Get in touch with Guardians of the Galaxy to locate Thanos and all infinity stones",
   project_id: Project.find_by_title("Infinity Stones").id,
   creator_id: 1,
@@ -181,14 +349,6 @@ Task.create({
 })
 
 # Add project Avengers Tower Tasks
-Task.create({
-  title: "Finish designing newest avengers tower",
-  project_id: Project.find_by_title("Avengers Tower").id,
-  creator_id: 1,
-  assignee_id: User.find_by_fname("Guest").id,
-  due_date: 5.days.ago.localtime
-})
-
 Task.create({
   title: "Don't mess with Hulk's cookies",
   project_id: Project.find_by_title("Avengers Tower").id,
